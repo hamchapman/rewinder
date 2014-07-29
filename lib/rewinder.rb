fd6b7c6068c751e1798cf17a449c22b85b6f80cd
Hamilton Chapman <hamchapman@gmail.com>
2014-07-29 16:49:04 +0100

    Moved stuff

--- lib/rewinder.rb
+++ lib/rewinder.rb
@@ -1,23 +1,38 @@
+require 'git'
+
 module Rewinder
-  # def self.root
-  #   puts 'Walking up dir tree'
-  #   loop do
-  #     cur = File.expand_path('.')
-  #     nxt = File.expand_path('..', cur)
-  #     puts cur
-  #     puts nxt
-  #     if File.directory?(.)
-  #       puts "I'm in a direc"
 
-  #     if File.directory?(File.expand_path('.git')) || nxt == cur
-  #       return # found root or git dir
-  #     end
-  #     Dir.chdir(nxt)
-  #   end
-  #   puts Dir.pwd
-  # end
+  def h
+    Dir.chdir("./")
 
-  # def change_dir_to_root_or_repo!
+    g = Git.open('./')
+    g.checkout(g.branch('master'))
+    g.branch('rewinder').checkout
 
-  # end
+    g.log(3).to_a.reverse.each_with_index do |l, i|
+      g.checkout(l.sha)
+      puts l.sha
+      `rake db:reset RAILS_ENV=production`
+      `rake db:migrate RAILS_ENV=production`
+      system "rails server &"
+      sleep 8
+      `phantomjs ./rasterize.js 'http://localhost:3000/' ~/.rewinder/homescreen#{(i+1).to_s.rjust(6, '0')}.png`
+      puts "Killing"
+      puts system 'kill -9 `cat tmp/pids/server.pid`'
+      puts "Hard resetting"
+      g.reset_hard(l)
+    end
+
+    g.checkout(g.branch('master'))
+    g.branch('rewinder').delete
+
+
+    `ffmpeg -f image2 -r 0.5 -i ~/.rewinder/homescreen%06d.png -s 1440x900 -vcodec libx264 -pix_fmt yuv420p ./rewinder.mp4`
+  end
+
+
+  # brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools
+
+  # https://github.com/schacon/ruby-git
+
 end
